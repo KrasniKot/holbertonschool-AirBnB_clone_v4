4566d2e394d3449cc5301302129bd9dd0519c4dd
KrasniKot <emanueltrias9@gmail.com>
2023-09-25 16:28:47 +0000

    task 1: cache_id added as a query string in the 0....html file

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -36,8 +36,9 @@ def hbnb():
 
     return render_template('0-hbnb.html',
                            states=st_ct,
-                           amenities=amenities,
-                           places=places)
+                           amenities=ameinities,
+                           places=places,
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":

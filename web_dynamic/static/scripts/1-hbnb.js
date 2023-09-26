0292c2a5c83a10a15b9b61451f748f60508dc296
KrasniKot <emanueltrias9@gmail.com>
2023-09-26 18:45:16 +0000

    task 2: script that updates the h4 title of amenities with the selected amenities almost done

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,10 @@
 $(() => {
+  const chckd = {}
+
   $('input').change(() => {
-    if (this.checked) {} // code goes here 
+    if (this.checked) { chckd[this.dataset.id] = this.dataset.name; }
+    else { delete chkd[this.dataset.id]; }
+
+    $('.amenities h4').text(chckd); // this is not expected to work properly but to be checked and fixed later
   });
 });

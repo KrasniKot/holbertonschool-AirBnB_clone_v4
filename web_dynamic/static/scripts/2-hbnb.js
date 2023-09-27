da8f17d720ab903d4807d970396ff6314499bb79
KrasniKot <emanueltrias9@gmail.com>
2023-09-27 06:54:52 -0700

    task 2: html checkbox fixed, script finished; task 3: html done, css done, script almost done

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,4 +8,11 @@ $(() => {
     }
     $('.amenities h4').text(Object.keys(chckd).join(', '));
   });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: (data, tstatus, xhr) => { 
+      if (tstatus === "OK") { $('#api_status').addClass('available'); }
+      else { $('#api_status').removeClass('available'); }
+  });
 });
